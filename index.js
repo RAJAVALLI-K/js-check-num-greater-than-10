0534d3a9284f9e3f97c349ef8bfab3ebf139e9e1
RAJAVALLI-K <rajavalli1932bca@gmail.com>
2023-03-09 20:48:44 +0530

    add js-check num large than 10

---
+++ index.js
@@ -0,0 +1,10 @@
+var number=prompt('enter a number:');
+if(number>0){
+    alert('number is greater than 10');
+}
+else if(number<0){
+    alert('number is lessthan 0');
+}
+else{
+    alert('number is zero');
+}
